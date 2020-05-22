f92bd233fc21cfbc0d166547a20ff905aac0553f
Karen Li <fwoshy@gmail.com>
2020-05-22 14:43:22 -0700

    another build test

--- commands/addy.js
+++ commands/addy.js
@@ -3,6 +3,6 @@ module.exports = {
 	description: 'addyyyy',
 	cooldown: 5,
 	execute(message, args) {
-		message.channel.send("kawaii~", {files: ["https://media.discordapp.net/attachments/243453768862138368/260868377474629632/IMAG0047.jpg?width=225&height=300"]});
+		message.channel.send("kawaii~", {files: ["https://media.discordapp.net/attachments/243453768862138368/260868377474629632/IMAG0047.jpg"]});
 	},
 };
