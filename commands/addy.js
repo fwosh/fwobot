4af8d1b00228979e2156a0cf2a0535bd805d487b
Karen Li <fwoshy@gmail.com>
2020-05-22 14:26:32 -0700

    build test with addy's beautiful face

---
+++ commands/addy.js
@@ -0,0 +1,8 @@
+module.exports = {
+	name: 'addy',
+	description: 'addyyyy',
+	cooldown: 5,
+	execute(message, args) {
+		message.channel.send("kawaii~", {files: ["https://media.discordapp.net/attachments/243453768862138368/260868377474629632/IMAG0047.jpg?width=225&height=300"]});
+	},
+};
