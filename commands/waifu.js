1cd02c8e444580a5f68f59f6b48699443170fe12
David Nguyen <nguyendavid3322@gmail.com>
2020-08-08 00:54:22 -0400

    fix waifu link

--- commands/waifu.js
+++ commands/waifu.js
@@ -1,10 +1,19 @@
+const Axios = require("axios");
+
 module.exports = {
 	name: 'waifu',
-  aliases: ['commands'],
+	aliases: ['commands'],
 	description: 'Generate a random waifu',
 	cooldown: 5,
 	execute(message, args) {
-    const randomNumber = Math.floor(Math.random() * 1000);
-		message.channel.send(`here\'s your waifu of the day uwu https://mywaifulist.moe/random?=${randomNumber}`);
+		const randomNumber = Math.floor(Math.random() * 1000);
+		Axios.get("https://mywaifulist.moe/random?=" + randomNumber)
+			.then((response) => {
+				console.log(response.request.res.responseUrl);
+				message.channel.send(`here\'s your waifu of the day uwu ` + response.request.res.responseUrl);
+			})
+			.catch((err) => {
+				console.error("Error: " + err);
+			});
 	},
 };
